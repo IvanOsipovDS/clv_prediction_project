345320e2d91d5bc50578114f03938c3a326d4434
IvanOsipovDS <Ivan.Osipov.job.mail@gmail.com>
2025-05-14 15:32:22 -0300

    Daily commit 3.2

--- src/data_preparation/prepare_data.py
+++ src/data_preparation/prepare_data.py
@@ -6,9 +6,9 @@ def load_raw_data() -> pd.DataFrame:
     purchases = pd.read_csv('data/raw/amazon-purchases.csv')
     survey = pd.read_csv('data/raw/survey.csv')
 
-    purchases['Order date'] = pd.to_datetime(purchases['Order date'])
+    purchases['Order date'] = pd.to_datetime(purchases['Order Date'])
     purchases['CustomerID'] = purchases['Survey ResponseID']
-    purchases['TotalPrice'] = purchases['Purchase price per unit'] * purchases['Quantity']
+    purchases['TotalPrice'] = purchases['Purchase Price Per Unit'] * purchases['Quantity']
 
     df = purchases.merge(survey, on='Survey ResponseID', how='left')
     return df
